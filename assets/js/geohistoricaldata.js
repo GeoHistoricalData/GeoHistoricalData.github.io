85ab47d8f9c1f51fdc90f7a785874a22838ffae6
HueyNemud <HueyNemud@users.noreply.github.com>
2017-10-24 18:43:38 +0200

    Update geohistoricaldata.js

--- assets/js/geohistoricaldata.js
+++ assets/js/geohistoricaldata.js
@@ -245,7 +245,7 @@ var options = {
     collapsed: isCollapsed
 };
 
-updateLayers("france");
+updateLayers("level_france");
 var layerControl = L.control.groupedLayers(baseLayers, groupedOverlays, options).addTo(map);
 
 function updateControlAndLayers(level) {
